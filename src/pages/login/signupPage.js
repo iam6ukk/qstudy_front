e02d4faf54cf88bea8b31edcd6d3760c60d1eb7f
iam6ukk <kyk001020@naver.com>
2024-01-15 00:00:17 +0900

    회원가입 페이지

--- src/pages/login/signupPage.js
+++ src/pages/login/signupPage.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React from "react";
+import styles from "./css/signup.module.css";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
-    return (
-        <div>
-            <form>
-                <div>
-                    <div>
-                        <label>아이디</label>
-                        <input type="id" value="id"/>
-                    </div>
-                    <div>
-                        <label>비밀번호</label>
-                        <input type="password" value="password"/>
-                    </div>
-                    <div>
-                        <label>비밀번호 확인</label>
-                        <input type="password" value="passwordConfirm"/>
-                    </div>
-                    <div>
-                        <label>닉네임</label>
-                        <input type="text" value="nickname"/>
-                    </div>
-                    <div>
-                        <label>이메일</label>
-                        <input type="email" value="email"/>
-                    </div>
+  const navigate = useNavigate();
 
-                    <div>
-                        <input type="submit" value="회원가입"/>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form}>
+        <div className={styles.basic_wrap}>
+          <div className={styles.signup_input}>
+            <label>ID</label>
+            <input type="id" value="id" />
+          </div>
+          <div className={styles.signup_input}>
+            <label>Password</label>
+            <input type="password" value="password" />
+          </div>
+          <div className={styles.signup_input}>
+            <label>Nickname</label>
+            <input type="text" value="nickname" />
+          </div>
+          <div className={styles.signup_input}>
+            <label>Email</label>
+            <input type="email" value="email" />
+          </div>
+
+          <div className={styles.signup_btn}>
+            <input
+              type="submit"
+              value="회원가입"
+              className={styles.submit}
+              onClick={() => {
+                navigate("/login");
+                alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+              }}
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
-
