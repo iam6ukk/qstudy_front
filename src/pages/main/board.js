ad85d6ab767b498fa7d5423aa5f5e4dd0290b5fc
iam6ukk <kyk001020@naver.com>
2024-01-16 22:22:27 +0900

    textarea 수정

--- src/pages/main/board.js
+++ src/pages/main/board.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import Calendar from "react-calendar";
 import styled from "styled-components";
 import "react-calendar/dist/Calendar.css";
 import styles from "./css/board.module.css";
+import GroupList from "../../components/group/myGroupList";
 
 export const CalendarBox = styled.div`
   display: flex;
@@ -51,21 +52,12 @@ export const StyleCalendar = styled(Calendar)`
 const Board = () => {
   const [today, setToday] = useState(new Date());
 
-  const people = [
-    "Creola Katherine Johnson: mathematician",
-    "Mario José Molina-Pasquel Henríquez: chemist",
-    "Mohammad Abdus Salam: physicist",
-    "Percy Lavon Julian: chemist",
-    "Subrahmanyan Chandrasekhar: astrophysicist",
-  ];
-  const listItems = people.map((person) => <li>{person}</li>);
-
   return (
     <div className={styles.board_container}>
       <div className={styles.board_sub}>
         <div className={styles.board_todo}>
           <label>내 할일</label>
-          <textarea name="todo" row={5} cols={40}></textarea>
+          <textarea name="todo"></textarea>
         </div>
         <div className={styles.board_calendar}>
           <CalendarBox>
@@ -75,7 +67,7 @@ const Board = () => {
       </div>
       <div className={styles.board_groups}>
         <lael>스터디 그룹 목록</lael>
-        <ul>{listItems}</ul>
+        <GroupList></GroupList>
       </div>
     </div>
   );
