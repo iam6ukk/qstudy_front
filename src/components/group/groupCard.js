50a5b4b195b2e5305387625051add2444e3ea645
hdev1004 <h.dev@kakao.com>
2024-01-30 00:10:31 +0900

    그룹 카드 왕관 설정

--- src/components/group/groupCard.js
+++ src/components/group/groupCard.js
@@ -2,9 +2,12 @@ import { useState, useRef, useEffect } from "react";
 import styled from "./css/group.module.css";
 import { useNavigate } from "react-router-dom";
 import "./css/groupGlobal.css";
+import Crown from "../../assets/crown.png";
+import { useCookies } from "react-cookie";
 
 const GroupCard = ({ item, url, openModal, setOpenModal, setData }) => {
   const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies();
   const showModal = () => {
     if (url === "my") {
       navigate("/main/my/group", { state: { groupId: item.group_id } });
@@ -13,6 +16,14 @@ const GroupCard = ({ item, url, openModal, setOpenModal, setData }) => {
       setData(item);
     }
   };
+
+  useEffect(() => {
+    if(cookies["login"] === undefined) {
+      alert("로그인이 필요합니다");
+      navigate('/login')
+      return;
+    }
+  }, [])
 
   const mouseMove = (e) => {};
   return (
@@ -25,6 +36,9 @@ const GroupCard = ({ item, url, openModal, setOpenModal, setData }) => {
       <div className={styled.title}>{item.title}</div>
       <div className={styled.contents}>{item.memo}</div>
       <div className={styled.writer}>작성자 : {item.writer}</div>
+      <div className={styled.crown}>
+        <img src={Crown}></img>
+      </div>
       <div
         className={styled.color}
         style={{ backgroundColor: item.color }}
