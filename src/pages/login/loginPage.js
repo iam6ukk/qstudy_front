401e46d93f6ea83dfb92747241cfcea8002cc80b
iam6ukk <kyk001020@naver.com>
2024-02-07 00:23:26 +0900

    로그인 구현

--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -1,71 +1,148 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import GoogleButton from "../../components/google.js"
-import GithubButton from "../../components/github.js"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import GoogleButton from "../../components/google.js";
+import GithubButton from "../../components/github.js";
 
-import styles from './css/login.module.css'
-import { useCookies } from 'react-cookie';
+import styles from "./css/login.module.css";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 const LoginPage = () => {
-    const [cookies, setCookie] = useCookies();
-    const navigate = useNavigate();
-    const codeCheck = async (location) => {
+  const [cookies, setCookie] = useCookies();
+  const [id, setId] = useState();
+  const [pwd, setPwd] = useState();
+  const navigate = useNavigate();
 
-        let params = new URLSearchParams(location.search);
-        let code = params.get("code");
-        if(code === null) return;
+  const idChange = (e) => {
+    setId(e.target.value);
+  };
+  const pwdChange = (e) => {
+    setPwd(e.target.value);
+  };
+  const codeCheck = async (location) => {
+    let params = new URLSearchParams(location.search);
+    let code = params.get("code");
+    if (code === null) return;
 
-        let response = await fetch(`${process.env.REACT_APP_DEV_PATH}/login/github?code=${code}`, {
-            method: "GET"
-        });
-        let json = await response.json();
-        const time = 3600; //1시간
-        const expiration = new Date(Date.now() + time * 1000);
-
-        setCookie("login", {
-            id: json.id,
-            picture: json.avatar_url,
-            nickname: json.login,
-            email: json.email
-        }, {
-            path: "/",
-            expires: expiration
-        })
-        console.log("Setup Cookie");
-        navigate("/main/all")
-    }
-
-    useEffect(() => {
-      codeCheck(window.location)
-    }, [])
-
-    return (
-        <div className={styles.login_container}>
-            
-            <div className={styles.login_form}>
-                <div className={styles.basic_wrap}>
-                    <div className={styles.login_input}>
-                        <label>ID</label>
-                        <input type="id" value="id"/>
-                    </div>
-                    <div className={styles.login_input}>
-                        <label>Password</label>
-                        <input type="password" value="password" />
-                    </div>
-                    
-                    <div className={styles.sign_wrap}>
-                        <input type="submit" value="로그인" className={styles.submit}/>
-                        <input type="submit" value="회원가입" className={styles.submit} onClick={() => {navigate('/signup')}}/>
-                    </div>
-                </div>
-                
-                <div className={styles.sso_wrap}>
-                    <GoogleButton />
-                    <GithubButton />
-                </div>
-            </div>
-        </div>
+    let response = await fetch(
+      `${process.env.REACT_APP_DEV_PATH}/login/github?code=${code}`,
+      {
+        method: "GET",
+      }
     );
+    let json = await response.json();
+    const time = 3600; //1시간
+    const expiration = new Date(Date.now() + time * 1000);
+
+    setCookie(
+      "login",
+      {
+        id: json.id,
+        picture: json.avatar_url,
+        nickname: json.login,
+        email: json.email,
+      },
+      {
+        path: "/",
+        expires: expiration,
+      }
+    );
+    console.log("Setup Cookie");
+    navigate("/main/all");
+  };
+
+  async function logIn() {
+    try {
+      const loginData = {
+        id: id,
+        password: pwd,
+      };
+      axios.post("http://localhost:8080/login", loginData).then((response) => {
+        console.log(response.data);
+
+        let res = response.data[0];
+
+        if (res.id === undefined) {
+          // id가 일치하지 않은 경우
+          console.log("아이디 불일치");
+          alert("입력하신 아이디가 일치하지 않습니다.");
+        } else if (res.pwd === null) {
+          // id는 일치하지만, pwd가 일치하지 않은 경우
+          // id가 일치하지 않은 경우
+          console.log("패스워드 불일치");
+          alert("입력하신 비밀번호가 일치하지 않습니다.");
+        } else if (res.id === id) {
+          // id, pwd가 모두 일치한 경우
+          console.log("로그인 성공");
+          const time = 3600; //1시간
+          const expiration = new Date(Date.now() + time * 1000);
+          //로그인 쿠키 설정
+          setCookie(
+            "login",
+            {
+              id: res.id,
+              picture: null,
+              nickname: res.nickname,
+              email: res.email,
+            },
+            {
+              path: "/",
+              expires: expiration,
+            }
+          );
+          console.log("Setup Cookie");
+          navigate("/main/all");
+        }
+      });
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  }
+
+  useEffect(() => {
+    codeCheck(window.location);
+  }, []);
+
+  return (
+    <div className={styles.login_container}>
+      <div className={styles.login_form}>
+        <div className={styles.basic_wrap}>
+          <div className={styles.login_input}>
+            <label>ID</label>
+            <input type="id" onChange={idChange} />
+          </div>
+          <div className={styles.login_input}>
+            <label>Password</label>
+            <input type="password" onChange={pwdChange} />
+          </div>
+
+          <div className={styles.sign_wrap}>
+            <input
+              type="submit"
+              value="로그인"
+              className={styles.submit}
+              onClick={() => {
+                logIn();
+              }}
+            />
+            <input
+              type="submit"
+              value="회원가입"
+              className={styles.submit}
+              onClick={() => {
+                navigate("/signup");
+              }}
+            />
+          </div>
+        </div>
+
+        <div className={styles.sso_wrap}>
+          <GoogleButton />
+          <GithubButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
