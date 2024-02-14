90b84181be7be438c5941a360fc31d76b695386b
hdev1004 <h.dev@kakao.com>
2024-02-14 23:01:28 +0900

    마이페이지 이미지 업로드 수정

--- src/pages/main/mypage.js
+++ src/pages/main/mypage.js
@@ -2,18 +2,34 @@ import React, { useEffect, useRef, useState } from 'react';
 import styles from './css/mypage.module.css';
 import Profile from "../../assets/profile.png";
 import { useCookies } from 'react-cookie';
+import imageCompression from "browser-image-compression";
 
 const MyPage = () => {
 
   const [cookies, setCookie] = useCookies();
   const fileInputRef = useRef(null); 
   const [formData, setFormData] = useState({
-    nickname: cookies["login"].nickname,
+    nickname: cookies["login"]?.nickname,
     password: '',
-    email: cookies["login"].email === null ? '' : cookies.email,
+    email: cookies["login"]?.email === null ||  cookies["login"]?.email === undefined ? '' : cookies["login"].email,
   });
 
-  const [changeImage, setChangeImage] = useState(undefined);
+  const [changeImage, setChangeImage] = useState(cookies["login"]?.picture);
+
+  const encodeFileToBase64 = async (fileBlob) => {
+    const options = {
+      maxSizeMB: 0.2, // 이미지 최대 용량
+      maxWidthOrHeight: 1920, // 최대 넓이(혹은 높이)
+      useWebWorker: true,
+    };
+    if(fileBlob === undefined || fileBlob === null) return;
+    const compressedFile = await imageCompression(fileBlob, options);
+    const promise = imageCompression.getDataUrlFromFile(compressedFile);
+    promise.then((result) => {
+      setChangeImage(result);
+      console.log(result);
+    })
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,9 +37,30 @@ const MyPage = () => {
   };
 
   const handleSubmit = () => {
-    console.log('변경된 사용자 정보:', formData);
-    // 여기서는 서버로 데이터를 보내는 코드를 추가해야 합니다.
-    // 실제로는 AJAX나 다른 방법을 사용하여 서버로 데이터를 전송해야 합니다.
+    if(formData.nickname.trim() === '') {
+      alert("닉네임을 입력해주세요.");
+      return;
+    }
+
+    if(formData.password.trim() === '') {
+      alert("비밀번호를 입력해주세요.");
+      return;
+    }
+
+    if(formData.email.trim() === '') {
+      alert("이메일을 입력해주세요.");
+      return;
+    }
+    if(window.confirm("저장하시겠습니까?")) {
+      let data = {
+        nickname: formData.nickname,
+        password: formData.password,
+        email: formData.email,
+        picture: changeImage
+      }
+
+      
+    }
   };
 
   return (
@@ -33,11 +70,11 @@ const MyPage = () => {
         <div className={styles.profile_Group}>
           <label htmlFor="profilePicture">프로필 사진 변경</label>
           <input name="profilePicture" ref={fileInputRef} type='file' accept='image/gif, image/jpeg, image/png' style={{display: "none"}}  onChange={(e) => { 
-            setChangeImage(URL.createObjectURL(e.target.files[0]))}}>
+           encodeFileToBase64(e.target?.files[0])}}>
           </input>
 
           
-          <img className={styles.profile_image} src={changeImage === undefined ? Profile : changeImage} onClick={() => {fileInputRef.current.click()}}></img>
+          <img className={styles.profile_image} src={changeImage === undefined || changeImage === null ? Profile : changeImage} onClick={() => {fileInputRef.current.click()}}></img>
           
         </div>
 
@@ -56,6 +93,7 @@ const MyPage = () => {
         <div className={styles.formGroup}>
           <label htmlFor="password" className={styles.label}>비밀번호 변경</label>
           <input
+            className={styles.inputForm}
             type="password"
             id="password"
             name="password"
@@ -66,6 +104,7 @@ const MyPage = () => {
         <div className={styles.formGroup}>
           <label htmlFor="email" className={styles.label}>이메일 변경</label>
           <input
+            className={styles.inputForm}
             value={formData.email}
             type="email"
             id="email"
@@ -74,7 +113,7 @@ const MyPage = () => {
           />
         </div>
 
-        <button type="button" onClick={handleSubmit} className={styles.button}>
+        <button className={styles.saveBtn} type="button" onClick={handleSubmit}>
           저장
         </button>
       </form>
